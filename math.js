29438607fa03d0ca3d7cad2031c4a069a6071c3f
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-02-06 22:37:44 -0600

    expanded 'library'

--- math.js
+++ math.js
@@ -4,4 +4,32 @@ math.add = function (first, second) {
 	return first + second;
 };
 
+math.subtract = function (first, second) {
+	return first - second;
+}
+
+math.multiply = function (first, second) {
+	return first*second;
+}
+
+math.divide = function (first, second) {
+	return first/second;
+}
+
+math.circle = {};
+
+math.circle.circumference = function (radius) {
+	return math.multiply(3.14159265358979, math.multiply(2, radius) );
+}
+
+math.circle.area = function (radius) {
+	return math.multiply(3.14159265358979, math.multiply(radius, radius) );
+}
+
+math.cylinder = {};
+
+math.cylinder.surfaceArea = function (radius, height) {
+	return math.add(math.multiply(2, math.circle.area(radius)), math.multiply(math.circle.circumference(radius), height) );
+}
+
 module.exports = math;
--- test.js
+++ test.js
@@ -10,5 +10,57 @@ describe("math", function () {
 			assert.equal(result, 15);
 		});
 	});
+
+	describe("subtract", function () {
+		it("should subtract the second number from the first", function () {
+			var result = math.subtract(10, 5);
+
+			assert.equal(result, 5);
+		});
+	});
+
+	describe("multiply", function () {
+		it("should multiply the first number by the second", function () {
+			var result = math.multiply(10, 5);
+
+			assert.equal(result, 50);
+		});
+	});
+
+	describe("divide", function () {
+		it("should divide the first number by the second", function () {
+			var result = math.divide(10, 5);
+
+			assert.equal(result, 2);
+		});
+	});
+
+	describe("circle", function () {
+		describe("circumference", function () {
+			it("should find the circumference of a circle", function () {
+				var result = math.circle.circumference(5);
+
+				assert.equal(result, 31.4159265358979);
+			});
+		});
+
+		describe("area", function () {
+			it("should find the area of a circle", function () {
+				var result = math.circle.area(10);
+
+				assert.equal(result, 314.159265358979);
+			});
+		});
+	});
+
+	describe("cylinder", function () {
+		describe("surfaceArea", function () {
+			it("should find the surface area of a cylinder", function () {
+				var result = math.cylinder.surfaceArea(10, 1);
+
+				assert.equal(result, 691.1503837897537);
+			});
+		});
+	});
 	
 });
